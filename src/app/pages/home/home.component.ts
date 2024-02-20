f353c2722cfa5fd94426423dc37580925a1f991b
rri <raul.rincones@adorsys.com>
2024-02-20 19:08:14 +0100

    progressing delete update create tasks with signals and keycloak update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, signal, WritableSignal} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {Task} from "../../models/task.model";
 
 @Component({
   selector: 'app-home',
@@ -9,28 +10,60 @@ import {CommonModule} from '@angular/common';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  tasks: WritableSignal<{ id: number; tittle: string }[]> = signal([
-    {id: 1, tittle: 'Lazy Loading'},
-    {id: 2, tittle: 'Defer'},
-    {id: 3, tittle: 'Runtime'},
+  tasks: WritableSignal<Task[]> = signal([
+    {id: Math.floor(Math.random() * 10000), title: 'Lazy Loading', completed: false},
+    {id: Math.floor(Math.random() * 10000), title: 'Defer', completed: false},
+    {id: Math.floor(Math.random() * 10000), title: 'Runtime', completed: false},
   ]);
   newTaskTitle: WritableSignal<string> = signal('');
+  editToggle: WritableSignal<boolean> = signal(false);
+  selectedId: WritableSignal<number> = signal(0);
 
-  createTaskHandler(event: any) {
+
+  settingTask(event: Event) {
     const value = event.target as HTMLInputElement;
-
     this.newTaskTitle.set(value.value)
+  }
 
-    const newTaskValue: { id: number; tittle: string } = {
+  createTaskHandler(event: Event) {
+    const value = event.target as HTMLInputElement;
+    this.newTaskTitle.set(value.value)
+    const newTaskValue: Task = {
       id: Math.floor(Math.random() * 10000),
-      tittle: this.newTaskTitle()
+      title: this.newTaskTitle(),
+      completed: false
     }
     this.tasks.update(tasks => [newTaskValue, ...tasks]);
     this.newTaskTitle.set('');
-
   }
 
-  deleteItemHandle(taskId: number) {
+  toggleEdit(taskId: number) {
+    this.editToggle.set(!this.editToggle());
+    this.selectedId.set(taskId)
+  }
+
+  updateTaskHandle(task: Task) {
+    // this.tasks.update(todos => {
+    //   return todos.map((todo) => {
+    //     if (todo.id === task.id) {
+    //       return {
+    //         ...todo,
+    //         completed: !todo.completed
+    //       }
+    //     }
+    //     return todo
+    //   })
+    // })
+    const newTask: Task = {
+      id: task.id,
+      title: task.title,
+      completed: true
+    }
+    this.tasks.update(tasks => tasks.map((todo: Task) => todo.id === task.id ? newTask : todo))
+    console.log(this.tasks())
+  }
+
+  deleteTaskHandle(taskId: number) {
     console.log(taskId)
     this.tasks.update(tasks => tasks.filter(task => task.id !== taskId))
   }
